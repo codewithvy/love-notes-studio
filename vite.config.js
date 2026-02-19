cb159d16770353fee8a1ddae9e4c08f969a53fef
Vy Ho <codewithvy@gmail.com>
2026-02-19 04:11:47 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Uncomment and update with your repo name when deploying to GitHub Pages
-  // base: '/valentine-card-maker/',
+  base: '/love-notes-studio/',
 })
